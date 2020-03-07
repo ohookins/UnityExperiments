cffa43cb474e0a900995cc5f55a4c12607702c8d
Oliver Hookins <ohookins@atlassian.com>
2020-03-07 21:40:31 +1100

    End of activity in Experiment 11.3.

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class Functions : MonoBehaviour
 {
     string myText = "Game Started";
+    int answer;
 
     // Start is called before the first frame update
     void Start()
     {
         messenger();
+        answer = AddNumbers(5, 3);
+        Debug.Log(answer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void messenger()
     {
         Debug.Log(myText);
+    }
+
+    int AddNumbers(int num1, int num2)
+    {
+        return num1 + num2;
     }
 }
