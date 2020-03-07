d374a414db30fba618be3cc9036b7942c71241be
Oliver Hookins <ohookins@atlassian.com>
2020-03-08 10:34:01 +1100

    End of activity in Experiment 13.1.

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+	// Use this for initialization	
+	void Start()
+	{
+
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	void OnCollisionEnter(Collision info)
+    {
+		if (info.gameObject.name == "Plane")
+		{
+			Destroy(gameObject);
+		}
+
+		if (info.gameObject.name == "Sphere")
+		{
+			gameObject.GetComponent<Rigidbody>().AddForce(0, 300, 0);
+		}
+	}
+}
