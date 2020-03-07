11d2128e3af3beccbf654e8ef182d48850fea4a3
Oliver Hookins <ohookins@atlassian.com>
2020-03-07 21:24:17 +1100

    End of activity in Experiment 11.1.

---
+++ Assets/Scripts/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions : MonoBehaviour
+{
+    string myText = "Game Started";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        messenger();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void messenger()
+    {
+        Debug.Log(myText);
+    }
+}
