36046a5f8ca6b431644d7761e4757beb24a6a5c0
Oliver Hookins <ohookins@atlassian.com>
2020-03-08 19:33:58 +1100

    End of activity in Experiment 17.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,6 +7,7 @@ public class Spawner : MonoBehaviour
     [Tooltip("This needs to be a prefab not already in the scene," +
         " otherwise the original cube is deleted and cannot be spawned anymore by this script.")]
     public GameObject theCube;
+    GameObject newObject;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,8 @@ public class Spawner : MonoBehaviour
     {
         while (true)
         {
-            Instantiate(theCube, transform.position, transform.rotation);
+            newObject = Instantiate(theCube, transform.position, transform.rotation);
+            Destroy(newObject, 1.0f);
             yield return new WaitForSeconds(Random.Range(0.1f, 5.0f));
         }
     }
